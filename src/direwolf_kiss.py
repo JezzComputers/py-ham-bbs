bc77c28c4c61518b411e0dacfee73a31ed715bf4
Ethan <ethan@kinghavok.net>
2026-03-10 17:55:39 +1100

    added msg specific user need to make it only see msg to yourself

--- src/direwolf_kiss.py
+++ src/direwolf_kiss.py
@@ -42,7 +42,6 @@ def listener(rx_socket: socket.socket, builder: AX25FrameBuilder) -> None:
         try:
             data: bytes = rx_socket.recv(4096)
             if not data:
-                print(f"{MAGENTA}RX socket closed{RESET}")
                 break
 
             print(f"\n{CYAN}[RX RAW]{RESET} {data.hex()}")
@@ -60,6 +59,8 @@ def listener(rx_socket: socket.socket, builder: AX25FrameBuilder) -> None:
             print(f"{MAGENTA}RX error:{RESET} {e}")
             break
 
+config = AX25Config("NOCALL", 0, "VK3ETH", 0)
+builder = AX25FrameBuilder(config)
 
 def main() -> None:
     use_color()
@@ -71,8 +72,8 @@ def main() -> None:
         print(f"{MAGENTA}KISS connect failed:{RESET} {e}")
         sys.exit(1)
 
-    # Configure addresses here
-    config = AX25Config("VK3ETH", 0, "VK3JEZ", 0)
+    # default builder so listener can start
+    config = AX25Config("NOCALL", 0, "VK3ETH", 0)
     builder = AX25FrameBuilder(config)
 
     threading.Thread(target=listener, args=(rx_socket, builder), daemon=True).start()
@@ -82,13 +83,25 @@ def main() -> None:
 
     while True:
         try:
-            msg = input(">>> ")
-            if msg.strip():
-                send_frame(tx_socket, msg, builder)
+            msg = input("MESSAGE >>> ").strip()
+            
+            print("Who would you like this message to go to")
+            user = input("CALLSIGN >>> ").strip().upper()
+
+            if not (user.startswith("VK3") and len(user) == 6):
+                print("Invalid user try again")
+                continue
+
+            config = AX25Config(user, 0, "VK3ETH", 0)
+            builder = AX25FrameBuilder(config)
+
+            send_frame(tx_socket, msg, builder)
+
         except KeyboardInterrupt:
             print("\nExiting.")
             sys.exit(0)
 
 
+            
 if __name__ == "__main__":
     main()
