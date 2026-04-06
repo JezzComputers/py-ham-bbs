346a356b46febaaddd34db4e5e2a742d669fb3e4
Jeremy <jeremy_sprague@outlook.com>
2026-04-06 16:48:23 +1000

    refactor: update dependencies to use uuid6 and adjust server/client ports

--- src/server.py
+++ src/server.py
@@ -1,33 +1,38 @@
 import asyncio
-from datetime import datetime, UTC
 from websockets.asyncio.server import serve, ServerConnection
-import logging
-from uuid import uuid7
+from websockets.exceptions import ConnectionClosed
+from datetime import datetime
+from zoneinfo import ZoneInfo
 import json
+import logging
+
+from platform import python_version_tuple
+if int(python_version_tuple()[1]) < 14:
+	from uuid6 import uuid7  # pyright: ignore[reportMissingImports, reportUnknownVariableType]
+else:
+	from uuid import uuid7
+
 
 logger = logging.getLogger(__name__)
 
+
 async def handler(websocket: ServerConnection) -> None:
-    logger.log(logging.INFO, "Client connected: %s", websocket.remote_address)
-    
-    try:
-        while True:
-            await websocket.send(json.dumps({
-                "timestamp": datetime.now(UTC).isoformat(),
-                "uuid": str(uuid7())
-            }))
-            await asyncio.sleep(1)
-    
-    
-    except Exception as e:
-        logger.log(logging.ERROR, "Error in handler: %s", e)
-    finally:
-        logger.log(logging.INFO, "Client disconnected: %s", websocket.remote_address)
+	logger.log(logging.INFO, "Client connected: %s", websocket.remote_address)
+	try:
+		while True:
+			await websocket.send(json.dumps({"timestamp": datetime.now(ZoneInfo("Australia/Melbourne")).isoformat(), "uuid": str(uuid7())}))  # pyright: ignore[reportUnknownArgumentType]
+			await asyncio.sleep(1)
+	except ConnectionClosed as e:
+		logger.log(logging.INFO, "Connection closed: %s", e)
+	finally:
+		logger.log(logging.INFO, "Client disconnected: %s", websocket.remote_address)
 
 
 async def main() -> None:
-    async with serve(handler, 'localhost', 8765) as server:
-        print("Server started on ws://localhost:8765")
-        await server.serve_forever()
+	async with serve(handler, "0.0.0.0", 8765) as server:  # noqa: S104
+		print("Time server started on ws://0.0.0.0:8765")
+		await server.serve_forever()
 
-asyncio.run(main())
+
+if __name__ == "__main__":
+	asyncio.run(main())
--- src/websockets_demo/websoc_client.py
+++ src/websockets_demo/websoc_client.py
@@ -2,17 +2,16 @@ import asyncio
 from websockets.asyncio.client import connect
 
 
-async def listen_for_time() -> None:
-	# Explicitly using the rework's 'connect'
-	async with connect("ws://localhost:8888") as websocket:
-		print("Connected to ws://localhost:8888")
+async def listen_sock() -> None:
+	async with connect("ws://localhost:8765") as websocket:
+		print("Connected to ws://localhost:8765")
 
 		async for message in websocket:
-			print(f"The server says the time is: {message}")
+			print(f"The server says: {message}")
 
 
 if __name__ == "__main__":
 	try:
-		asyncio.run(listen_for_time())
+		asyncio.run(listen_sock())
 	except KeyboardInterrupt:
 		print("\nClient stopped.")
